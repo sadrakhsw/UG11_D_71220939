8a0d2c6bace321536e9d774d491a2c9d16600819
sadrakhsw <117900802+sadrakhsw@users.noreply.github.com>
2022-12-01 16:21:40 +0700

    Add files via upload

---
+++ 1_UG11_71220939.py
@@ -0,0 +1,8 @@
+print=("Halo selamat datang di bioskop!")
+print=("Dimanakah kamu ingin menonton?")
+def tempat():
+    print("1) XXI Empire")
+    print("2) XXI Amplaz")
+    print("3) XXI JCM")
+
+
